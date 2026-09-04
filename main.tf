63bfd01af9326822d357c731ca1266e67d5c0734
miyoming12 <romeo.nagajr@gmail.com>
2026-09-05 01:25:23 +0800

    Asdasd@asd

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  name                = var.nsg
+  name                = var.nsg_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
