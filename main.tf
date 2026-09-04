c2994e71defafacbf5dd7abb72e1c83bb4a8019f
miyoming12 <romeo.nagajr@gmail.com>
2026-09-04 22:34:34 +0800

    Asdasd@

--- main.tf
+++ main.tf
@@ -5,22 +5,134 @@ terraform {
       version = "5.2.0"
     }
   }
-
-  backend "azurerm" {
+backend "azurerm" {
     resource_group_name  = ""
     storage_account_name = ""
     container_name       = ""
     key                  = ""
 
 }
-
 }
 provider "azurerm" {
   features {}
 }
 
 
+
+
+/* variable "prefix" {
+  default = "tfvmex"
+} */
+
+
+data "azurerm_virtual_network" "existing" {
+  name                = "miyoming_vnet"
+  resource_group_name = "vnet"
+}
+
+data "azurerm_subnet" "existing_subnet" {
+  name                 = "default"
+  virtual_network_name = data.azurerm_virtual_network.existing.name
+  resource_group_name  = data.azurerm_virtual_network.existing.resource_group_name
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = "southeastasia"
 }
+
+resource "azurerm_network_security_group" "example" {
+  name                = var.nsg
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  dynamic "security_rule" {
+    for_each = var.nsg_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+    source_port_range          = security_rule.value.source_port_range
+    destination_port_range     = security_rule.value.destination_port_range
+    source_address_prefix      = security_rule.value.source_address_prefix
+    destination_address_prefix = security_rule.value.destination_address_prefix
+    }
+  }
+
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = data.azurerm_subnet.existing_subnet.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+
+
+
+/* resource "azurerm_public_ip" "example" {
+  name                = "samplevm-public-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = data.azurerm_subnet.existing_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+
+
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B1s"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-miyoming"
+    admin_username = "miyoming"
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+#  tags = {
+#    environment = "staging"
+#  }
+} */
--- variables.tf
+++ variables.tf
@@ -1,4 +1,32 @@
-variable "prefix" {
-  default = "tfvmex"
+/* variable "admin_password" {
+  description = "The password for the admin user"
+  type        = stringzdad
+} */
+
+variable "nsg_name" {
+  description = "The name of the network security group"
+  type        = string
+  
 }
+
+variable "prefix" {
+  description = "The prefix for resource names"
+  type        = string
+}
+
+variable "nsg_rules" {
+  description = "A list of network security group rules"
+  type = list(object({
+    name      = string
+    priority  = number
+    direction = string
+    access    = string
+    protocol  = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+}
+
 
